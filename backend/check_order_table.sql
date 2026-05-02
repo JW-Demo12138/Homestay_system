9721fe5bc7cf46e1428321a12723ad3975b34682
JW-Demo12138 <1216839016@qq.com>
2026-05-02 21:47:44 +0800

    保存一下

--- backend/check_order_table.sql
+++
@@ -1,2 +0,0 @@
-USE homestay;
-DESCRIBE `order`;
--- backend/fix_column_name.sql
+++
@@ -1,7 +0,0 @@
-USE homestay;
-
--- 先删除错误的字段
-ALTER TABLE user DROP COLUMN ame;
-
--- 重新添加正确的字段
-ALTER TABLE user ADD name VARCHAR(50) DEFAULT NULL COMMENT '姓名' AFTER phone;
